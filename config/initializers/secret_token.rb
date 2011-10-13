0eeed271ed8b4b126a74dd7f6bc4e8a88124ef85
Damien Bachet <onewaytodaf@gmail.com>
2011-10-13 11:44:29 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaquetteCreatePostAsGuestOrUserWithAjax::Application.config.secret_token = '529b60a1030d5e553d71d5aee30e1db479252945d95fd5a2dc8b12ef180501cd7d7a261981066164c083b18f4b291f5b1915b516fc7eb42d91727f8fa497eae4'
