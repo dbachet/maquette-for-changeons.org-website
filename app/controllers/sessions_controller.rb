c91d5d732f3a3af55799f7da7febb873c8738674
Damien Bachet <onewaytodaf@gmail.com>
2011-10-17 21:19:46 +0200

    the fancy login for comment is showing login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,42 +1,95 @@
 class SessionsController < Devise::SessionsController
-  # layout false
+    ###########################################
+    # To add in config/initializers/devise.rb #
+    ###########################################
+    
+    # config.http_authenticatable_on_xhr = false
+    # config.navigational_formats = [:html, :json]
+    #*********************************************#
   
   def create
-      resource = warden.authenticate!(:scope => resource_name, :recall => "sessions/failure")
-      
-      # SHOULDN'T BE HERE
-      @comment = Comment.new
-      # SHOULDN'T BE HERE
-      
-      return sign_in_and_redirect(resource_name, resource)
+    resource = warden.authenticate!(:scope => resource_name, :recall => "sessions#failure")
+    !resource.nil? ? @result = true : @result = false
+    
+    # SHOULDN'T BE HERE
+    @comment = Comment.new
+    # SHOULDN'T BE HERE
+    
+    return sign_in_and_redirect(resource_name, resource)
   end
-
+  
   def fancy_comment_login
     resource = build_resource
     clean_up_passwords(resource)
-    respond_with_navigational(resource, stub_options(resource)){ render :fancy_new, :layout => false }
+    respond_with_navigational(resource, stub_options(resource)){ render :fancy_comment_login, :layout => false }
   end
   
   def sign_in_and_redirect(resource_or_scope, resource=nil)
     scope = Devise::Mapping.find_scope!(resource_or_scope)
     resource ||= resource_or_scope
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(scope, resource) unless warden.user(scope) == resource
-    puts "ça marche"
     
-    # return render :js => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
-    # return render :create 
-    respond_to do |format|
-      format.js
-    end
+    puts "Result -> #{@result}"
+    # return render :create => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
+    # return render :sign_in_and_redirect => {:redirect => :back}
+        respond_to do |format|
+          format.html { redirect_to(:back) }
+          format.js { render :sign_in_and_redirect }
+        end
   end
 
   def failure
-    puts "ça marche pas"
-    respond_to do |format|
-      format.js
-    end
-    # return render :js => {:success => false, :errors => ["Login failed."]}
+    # return render :json => {:success => false, :errors => ["Login failed."]}
   end
-
+  
 end
+
+# class SessionsController < Devise::SessionsController
+#   # layout false
+#   
+#   def create
+#       resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
+#       # puts "Resource -> #{resource}"
+#       
+#       # SHOULDN'T BE HERE
+#       @comment = Comment.new
+#       # SHOULDN'T BE HERE
+#       
+#       if !resource.nil?
+#         return sign_in_and_redirect(resource_name, resource)
+#       else
+#         return failure
+#       end
+#   end
+# 
+#   def fancy_comment_login
+#     resource = build_resource
+#     clean_up_passwords(resource)
+#     respond_with_navigational(resource, stub_options(resource)){ render :fancy_comment_login, :layout => false }
+#   end
+#   
+#   def sign_in_and_redirect(resource_or_scope, resource=nil)
+#     scope = Devise::Mapping.find_scope!(resource_or_scope)
+#     resource ||= resource_or_scope
+#     set_flash_message(:notice, :signed_in) if is_navigational_format?
+#     sign_in(scope, resource) unless warden.user(scope) == resource
+#     
+#     # return render :js => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
+#     # return render :create 
+#     respond_to do |format|
+#       format.js { render :sign_in_and_redirect }
+#     end
+#   end
+# 
+#   def failure
+#     @test_var = "The combination of email/password is incorrect"
+#     puts test_var
+#     flash[:alert] = "The combination of email/password is incorrect"
+#     # respond_to do |format|
+#     #       format.js { render :failure }
+#     #     end
+#     # return render :js => {:success => false, :errors => ["Login failed."]}
+#     return render :js
+#   end
+# 
+# end
